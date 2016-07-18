b515cc2a45eb4d2aae375ed0511efff03460d493
gma992 <gabrielmaldonado.art@gmail.com>
2016-07-18 12:18:57 +0200

    Trying out new way to get Yahoo info through xml

--- almond-stock-market-data.php
+++ almond-stock-market-data.php
@@ -3,7 +3,7 @@
 Plugin Name: [1.1]Almond Stock Market Data
 Plugin URI: http://almondwp.com
 Description: Display stock market data in your website using a widget. Clean and simple. Activate it under Appearance > Widgets > Available Widgets or clicking in "Settings" below the plugin name.
-Version: 1.0
+Version: 1.1
 Author: Gabriel Maldonado
 Author URI: http://almondwp.com
 License: GPL2
@@ -33,53 +33,53 @@ function show_widget_link_on_activation($widget_links) {
 }
 add_filter("plugin_action_links_".plugin_basename(__FILE__), 'show_widget_link_on_activation', 10, 5);
 
-function awp_debug(){
-	echo '<br>' . '///////////////////////////////////////////' . '<br>';
-}
+// function awp_debug(){
+// 	echo '<br>' . '///////////////////////////////////////////' . '<br>';
+// }
 
-/* content filter for [KO], find [KO] in the post content and change it */
-function content_filter_for_ko_functionality($content){
-	global $post;
-	global $wpdb;
-	//print_r($post->post_content);
-	$to_replace = $post->post_content;
-	$a = '[KO]';
-	$ticker = '<span id="new-hover-functionality">coca cola</span>';
-	if (strrpos($to_replace, $a)) {
+// /* content filter for [KO], find [KO] in the post content and change it */
+// function content_filter_for_ko_functionality($content){
+// 	global $post;
+// 	global $wpdb;
+// 	//print_r($post->post_content);
+// 	$to_replace = $post->post_content;
+// 	$a = '[KO]';
+// 	$ticker = '<span id="new-hover-functionality">coca cola</span>';
+// 	if (strrpos($to_replace, $a)) {
 		
-		echo '<strong> DEBUG: </strong> string to be replaced found!';
-		$content = str_replace($a, $ticker, $content);
-		awp_debug();
-		return $content;
+// 		echo '<strong> DEBUG: </strong> string to be replaced found!';
+// 		$content = str_replace($a, $ticker, $content);
+// 		awp_debug();
+// 		return $content;
 		
-		//return str_replace($to_replace, $a, $ticker);
+// 		//return str_replace($to_replace, $a, $ticker);
 
-	} else {
-		echo 'nope';
-	}
-	//return 'yay';
+// 	} else {
+// 		echo 'nope';
+// 	}
+// 	//return 'yay';
 
-	//echo 'HELLO';
+// 	//echo 'HELLO';
 
-}
-add_filter( 'the_content', 'content_filter_for_ko_functionality');
-//add_action( 'wp_head', 'content_filter_for_ko_functionality');
+// }
+// add_filter( 'the_content', 'content_filter_for_ko_functionality');
+// //add_action( 'wp_head', 'content_filter_for_ko_functionality');
 
-/* DEVELOPING SHORTCODE FOR SPAN TAGS */
-function gma_almond_stock_prices_shortcode_tags($attr){
+// /* DEVELOPING SHORTCODE FOR SPAN TAGS */
+// function gma_almond_stock_prices_shortcode_tags($attr){
 
-	switch ($attr) {
-		case 'test1':
-			echo 'test 1!!!';
-			break;
+// 	switch ($attr) {
+// 		case 'test1':
+// 			echo 'test 1!!!';
+// 			break;
 		
-		default:
-			echo 'default';
-			break;
-	}
+// 		default:
+// 			echo 'default';
+// 			break;
+// 	}
 
-}
-add_shortcode( 's', 'gma_almond_stock_prices_shortcode_tags' );
+// }
+// add_shortcode( 's', 'gma_almond_stock_prices_shortcode_tags' );
 /* // DEVELOPING SHORTCODE FOR SPAN TAGS */
 
 
@@ -101,66 +101,132 @@ class gma_almond_stock_prices extends WP_Widget{
 
 		function gma_almond_wp_http_api($each_ticker, $title, $instance, $args){
 			
-				
-			$http_req_test_url = 'http://finance.yahoo.com/webservice/v1/symbols/'.$each_ticker.'/quote?format=json';
 
+			/* yahoo deprecated the api, new way: */
+			/*
+			http://www.yqlblog.net/blog/2009/06/02/getting-stock-information-with-yql-and-open-data-tables/
+			https://github.com/josephdpurcell/YahooFinanceAPI/blob/master/YahooFinanceAPI.php
+
+			*/
+				
+			//$http_req_test_url = 'http://finance.yahoo.com/webservice/v1/symbols/'.$each_ticker.'/quote?format=json';
+			$http_req_test_url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22' . $each_ticker . '%22%29&env=store://datatables.org/alltableswithkeys';
 			$get_result = wp_remote_get( $http_req_test_url, $args );
 			$get_result_just_body = wp_remote_retrieve_body($get_result );
-
+			//gettype($get_result_just_body);
 			$the_json_object = json_decode($get_result_just_body);
 
-			if ($the_json_object->list->meta->count == '0') {
-				echo '<span id="almond-alert">The ticker "'.$each_ticker.'" does not exist or is not supported. Please doble check it in the widget dashboard or leave it blank. If you think it is a mistake please contact support.</span>';
+			//print_r($get_result);
+			//echo '======================';
+			//print_r($get_result['body']);
+			//echo '======================';
+			//print_r($get_result['response']);
+			$the_response = $get_result['body'];
+			echo 'type: ' . gettype($the_response);
+			echo $the_response;
+			//echo '======================';
 
-			} else {
-				$the_decoded_name_json_object = $the_json_object->list->resources[0]->resource->fields->name;
-				$the_decoded_symbol_json_object = $the_json_object->list->resources[0]->resource->fields->symbol;
-				$the_decoded_price_json_object = $the_json_object->list->resources[0]->resource->fields->price;
-				$the_decoded_volume_json_object = $the_json_object->list->resources[0]->resource->fields->volume;
-
-				$the_ticker = $the_decoded_symbol_json_object;
-				$the_price = substr($the_decoded_price_json_object, 0, 5) ;
-
-				$theTableThatGeneratesALink = $the_ticker;
-				$theNewWindowURL = "http://finance.yahoo.com/q?s=$each_ticker";
-				$theTableThatGeneratesTheVolume = '';
-
-				if ($instance['getLinks'] == true) {
-
-					$theTableThatGeneratesALink = 
-						"<a href='".$theNewWindowURL."' target='_blank'>".$the_ticker."</a>";
-				}
 			
-				if ($instance[ 'getGoogleOrYahoo' ] == true) {
-					
-					$theNewWindowURL = "https://www.google.com/finance?q=$each_ticker";	
-				}
-
-				if ($instance[ 'getTradingVolume' ] == true) {
-
-					$theTableThatGeneratesTheVolume = "<span id='almond-volume'>Volume: ".$the_decoded_volume_json_object." M.</span>";
-				}
-
-				echo "
-				<div class='wrap'>
-					<div id='almond-table-title'>
-						<span id='last-pull-comment'>".$the_decoded_name_json_object."</span>
-					</div>
-					<table id='almond-table'>
-						<tbody>
-							<tr>
-								<td>".$theTableThatGeneratesALink."
-								<td>".$the_price."</td>
-							</tr>
-						</tbody>
-					</table>
-					<div>
-						".$theTableThatGeneratesTheVolume."
-					</div>
-				</div>
-				";
-				
+			$the_regex = preg_match_all('/((.*?))/~', $the_response, $matches);	
+			//$the_regex_two = preg_match('/((.*?))/', $the_response);
+			//print_r($matches);
+			foreach ($matches as $key) {
+				print_r($key) . '<br>';
 			}
+			
+			//print_r($get_result['http_response']['body']);
+			// $feed = fetch_feed($http_req_test_url );
+			// print_r($feed);
+
+			// foreach ($feed as $item) {
+			// 	var_dump($item);
+			// }
+			//var_dump($the_json_object);
+			//echo gettype($the_json_object);
+			//var_dump($http_req_test_url);
+			//echo 'type: ' . gettype($get_result);
+			//echo 'dump: ' . var_dump($get_result);
+			//echo 'type: ' . gettype($get_result_just_body);
+			//echo 'dump: ' . var_dump($get_result_just_body);
+			//print_r($get_result_just_body);
+			//print_r($the_json_object);
+			//print_r(json_decode($get_result_just_body));
+			//$api_response = json_decode( wp_remote_retrieve_body( $get_result ) );
+			//var_dump($api_response);
+
+			// foreach ($get_result as $key) {
+			// 	echo $get_result[$key] . '<br>';
+			// }
+			//print_r($get_result);
+			//$feed = simplexml_import_dom($get_result);
+			// foreach ($feed['items'] as $item) {
+			// 	echo $item;
+			// }
+			// print_r($get_result);
+			// if (in_array('KO', $get_result)) {
+			// 	echo 'ko found!';
+			// } else {
+			// 	echo 'ko not found';
+			// }
+			
+//			https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
+
+//http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22ko%22%29&env=store://datatables.org/alltableswithkeys
+			
+			// if ($the_json_object->list->meta->count == '0') {
+			// 	var_dump($the_json_object);
+			// 	echo '<span id="almond-alert">The ticker "'.$each_ticker.'" does not exist or is not supported. Please doble check it in the widget dashboard or leave it blank. If you think it is a mistake please contact support.</span>';
+
+			// } else {
+			// 	var_dump($the_json_object);
+			// 	$the_decoded_name_json_object = $the_json_object->list->resources[0]->resource->fields->name;
+			// 	$the_decoded_symbol_json_object = $the_json_object->list->resources[0]->resource->fields->symbol;
+			// 	$the_decoded_price_json_object = $the_json_object->list->resources[0]->resource->fields->price;
+			// 	$the_decoded_volume_json_object = $the_json_object->list->resources[0]->resource->fields->volume;
+
+			// 	$the_ticker = $the_decoded_symbol_json_object;
+			// 	$the_price = substr($the_decoded_price_json_object, 0, 5) ;
+
+			// 	$theTableThatGeneratesALink = $the_ticker;
+			// 	$theNewWindowURL = "http://finance.yahoo.com/q?s=$each_ticker";
+			// 	$theTableThatGeneratesTheVolume = '';
+
+			// 	if ($instance['getLinks'] == true) {
+
+			// 		$theTableThatGeneratesALink = 
+			// 			"<a href='".$theNewWindowURL."' target='_blank'>".$the_ticker."</a>";
+			// 	}
+			
+			// 	if ($instance[ 'getGoogleOrYahoo' ] == true) {
+					
+			// 		$theNewWindowURL = "https://www.google.com/finance?q=$each_ticker";	
+			// 	}
+
+			// 	if ($instance[ 'getTradingVolume' ] == true) {
+
+			// 		$theTableThatGeneratesTheVolume = "<span id='almond-volume'>Volume: ".$the_decoded_volume_json_object." M.</span>";
+			// 	}
+
+			// 	echo "
+			// 	<div class='wrap'>
+			// 		<div id='almond-table-title'>
+			// 			<span id='last-pull-comment'>".$the_decoded_name_json_object."</span>
+			// 		</div>
+			// 		<table id='almond-table'>
+			// 			<tbody>
+			// 				<tr>
+			// 					<td>".$theTableThatGeneratesALink."
+			// 					<td>".$the_price."</td>
+			// 				</tr>
+			// 			</tbody>
+			// 		</table>
+			// 		<div>
+			// 			".$theTableThatGeneratesTheVolume."
+			// 		</div>
+			// 	</div>
+			// 	";
+				
+			//}
 		}
 
 		extract($args);
@@ -341,5 +407,3 @@ class gma_almond_stock_prices extends WP_Widget{
 
 	}
 }
-
-?>
